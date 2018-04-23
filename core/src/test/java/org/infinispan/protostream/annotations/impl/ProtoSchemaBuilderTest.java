d7b67d7a792490db273e7b16d9a6c25e5cf2edcc
Adrian Nistor <anistor@redhat.com>
2018-04-23 14:57:33 +0300

    IPROTO-53 ProtoSchemaBuilder fails for fields of type Byte[] (but byte[] works)

--- core/src/test/java/org/infinispan/protostream/annotations/impl/ProtoSchemaBuilderTest.java
+++ core/src/test/java/org/infinispan/protostream/annotations/impl/ProtoSchemaBuilderTest.java
@@ -363,17 +363,413 @@ public class ProtoSchemaBuilderTest extends AbstractProtoStreamTest {
       @ProtoField(number = 32/*, defaultValue = "1"*/)
       Date[] testField32 = {new Date(100), new Date(200)};
 
-      @ProtoField(number = 34/*, defaultValue = "1"*/)
-      Instant testField34 = Instant.ofEpochMilli(100);
+      @ProtoField(number = 33/*, defaultValue = "1"*/)
+      Instant testField33 = Instant.ofEpochMilli(100);
 
-      @ProtoField(number = 35/*, defaultValue = "1"*/)
-      Instant[] testField35 = {Instant.ofEpochMilli(100), Instant.ofEpochMilli(200)};
+      @ProtoField(number = 34/*, defaultValue = "1"*/)
+      Instant[] testField34 = {Instant.ofEpochMilli(100), Instant.ofEpochMilli(200)};
+
+      @ProtoField(number = 35, defaultValue = "35")
+      byte[] testField35 = {1, 2, 3};
 
       @ProtoField(number = 36, defaultValue = "36")
-      byte[] testField36 = {1, 2, 3};
+      Byte[] testField36 = {1, 2, 3};
 
-      //@ProtoField(number = 37, defaultValue = "37")
-      Byte[] testField37 = {1, 2, 3};
+      private char testField51;
+
+      @ProtoField(number = 51, defaultValue = "A")
+      public char getTestField51() {
+         return testField51;
+      }
+
+      public void setTestField51(char testField51) {
+         this.testField51 = testField51;
+      }
+
+      private char[] testField52 = {'a', 'b'};
+
+      @ProtoField(number = 52, defaultValue = "A")
+      public char[] getTestField52() {
+         return testField52;
+      }
+
+      public void setTestField52(char[] testField52) {
+         this.testField52 = testField52;
+      }
+
+      private Character testField53 = 'a';
+
+      @ProtoField(number = 53, defaultValue = "A")
+      public Character getTestField53() {
+         return testField53;
+      }
+
+      public void setTestField53(Character testField53) {
+         this.testField53 = testField53;
+      }
+
+      private Character[] testField54 = {'a', 'b'};
+
+      @ProtoField(number = 54, defaultValue = "A")
+      public Character[] getTestField54() {
+         return testField54;
+      }
+
+      public void setTestField54(Character[] testField54) {
+         this.testField54 = testField54;
+      }
+
+      private boolean testField55;
+
+      @ProtoField(number = 55, defaultValue = "A")
+      public boolean isTestField55() {
+         return testField55;
+      }
+
+      public void setTestField55(boolean testField55) {
+         this.testField55 = testField55;
+      }
+
+      private boolean[] testField56 = {true, true};
+
+      @ProtoField(number = 56, defaultValue = "true")
+      public boolean[] getTestField56() {
+         return testField56;
+      }
+
+      public void setTestField56(boolean[] testField56) {
+         this.testField56 = testField56;
+      }
+
+      private Boolean testField57 = true;
+
+      @ProtoField(number = 57, defaultValue = "true")
+      public Boolean getTestField57() {
+         return testField57;
+      }
+
+      public void setTestField57(Boolean testField57) {
+         this.testField57 = testField57;
+      }
+
+      private Boolean[] testField58 = {true, true};
+
+      @ProtoField(number = 58, defaultValue = "true")
+      public Boolean[] getTestField58() {
+         return testField58;
+      }
+
+      public void setTestField58(Boolean[] testField58) {
+         this.testField58 = testField58;
+      }
+
+      private short testField59;
+
+      @ProtoField(number = 59, defaultValue = "100")
+      public short getTestField59() {
+         return testField59;
+      }
+
+      public void setTestField59(short testField59) {
+         this.testField59 = testField59;
+      }
+
+      private short[] testField60 = {1, 2};
+
+      @ProtoField(number = 60, defaultValue = "100")
+      public short[] getTestField60() {
+         return testField60;
+      }
+
+      public void setTestField60(short[] testField60) {
+         this.testField60 = testField60;
+      }
+
+      private Short testField61 = 1;
+
+      @ProtoField(number = 61, defaultValue = "100")
+      public Short getTestField61() {
+         return testField61;
+      }
+
+      public void setTestField61(Short testField61) {
+         this.testField61 = testField61;
+      }
+
+      private Short[] testField62 = {1, 2};
+
+      @ProtoField(number = 62, defaultValue = "100")
+      public Short[] getTestField62() {
+         return testField62;
+      }
+
+      public void setTestField62(Short[] testField62) {
+         this.testField62 = testField62;
+      }
+
+      private float testField63;
+
+      @ProtoField(number = 63, defaultValue = "100.5")
+      public float getTestField63() {
+         return testField63;
+      }
+
+      public void setTestField63(float testField63) {
+         this.testField63 = testField63;
+      }
+
+      private float[] testField64 = {3.14f, 3.15f};
+
+      @ProtoField(number = 64, defaultValue = "100.5")
+      public float[] getTestField64() {
+         return testField64;
+      }
+
+      public void setTestField64(float[] testField64) {
+         this.testField64 = testField64;
+      }
+
+      private Float testField65 = 3.14f;
+
+      @ProtoField(number = 65, defaultValue = "100.5")
+      public Float getTestField65() {
+         return testField65;
+      }
+
+      public void setTestField65(Float testField65) {
+         this.testField65 = testField65;
+      }
+
+      private Float[] testField66 = {3.14f, 3.15f};
+
+      @ProtoField(number = 66, defaultValue = "100.5")
+      public Float[] getTestField66() {
+         return testField66;
+      }
+
+      public void setTestField66(Float[] testField66) {
+         this.testField66 = testField66;
+      }
+
+      private double testField67;
+
+      @ProtoField(number = 67, defaultValue = "100.5")
+      public double getTestField67() {
+         return testField67;
+      }
+
+      public void setTestField67(double testField67) {
+         this.testField67 = testField67;
+      }
+
+      private double[] testField68;
+
+      @ProtoField(number = 68, defaultValue = "100.5")
+      public double[] getTestField68() {
+         return testField68;
+      }
+
+      public void setTestField68(double[] testField68) {
+         this.testField68 = testField68;
+      }
+
+      private Double testField69 = 3.14;
+
+      @ProtoField(number = 69, defaultValue = "100.5")
+      public Double getTestField69() {
+         return testField69;
+      }
+
+      public void setTestField69(Double testField69) {
+         this.testField69 = testField69;
+      }
+
+      private Double[] testField70 = {3.14, 3.15};
+
+      @ProtoField(number = 70, defaultValue = "100.5")
+      public Double[] getTestField70() {
+         return testField70;
+      }
+
+      public void setTestField70(Double[] testField70) {
+         this.testField70 = testField70;
+      }
+
+      private int testField71;
+
+      @ProtoField(number = 71, defaultValue = "100")
+      public int getTestField71() {
+         return testField71;
+      }
+
+      public void setTestField71(int testField71) {
+         this.testField71 = testField71;
+      }
+
+      private int[] testField72;
+
+      @ProtoField(number = 72, defaultValue = "100")
+      public int[] getTestField72() {
+         return testField72;
+      }
+
+      public void setTestField72(int[] testField72) {
+         this.testField72 = testField72;
+      }
+
+      private Integer testField73 = 1;
+
+      @ProtoField(number = 73, defaultValue = "100")
+      public Integer getTestField73() {
+         return testField73;
+      }
+
+      public void setTestField73(Integer testField73) {
+         this.testField73 = testField73;
+      }
+
+      private Integer[] testField74 = {1, 2};
+
+      @ProtoField(number = 74, defaultValue = "100")
+      public Integer[] getTestField74() {
+         return testField74;
+      }
+
+      public void setTestField74(Integer[] testField74) {
+         this.testField74 = testField74;
+      }
+
+      private long testField75;
+
+      @ProtoField(number = 75, defaultValue = "100")
+      public long getTestField75() {
+         return testField75;
+      }
+
+      public void setTestField75(long testField75) {
+         this.testField75 = testField75;
+      }
+
+      private long[] testField76 = {1, 2};
+
+      @ProtoField(number = 76, defaultValue = "100")
+      public long[] getTestField76() {
+         return testField76;
+      }
+
+      public void setTestField76(long[] testField76) {
+         this.testField76 = testField76;
+      }
+
+      private Long testField77 = 1L;
+
+      @ProtoField(number = 77, defaultValue = "100")
+      public Long getTestField77() {
+         return testField77;
+      }
+
+      public void setTestField77(Long testField77) {
+         this.testField77 = testField77;
+      }
+
+      private Long[] testField78 = {1L, 2L};
+
+      @ProtoField(number = 78, defaultValue = "100")
+      public Long[] getTestField78() {
+         return testField78;
+      }
+
+      public void setTestField78(Long[] testField78) {
+         this.testField78 = testField78;
+      }
+
+      private String testField79 = "test";
+
+      @ProtoField(number = 79, defaultValue = "xyz")
+      public String getTestField79() {
+         return testField79;
+      }
+
+      public void setTestField79(String testField79) {
+         this.testField79 = testField79;
+      }
+
+      private String[] testField80 = {"one", "two"};
+
+      @ProtoField(number = 80, defaultValue = "xyz")
+      public String[] getTestField80() {
+         return testField80;
+      }
+
+      public void setTestField80(String[] testField80) {
+         this.testField80 = testField80;
+      }
+
+      private Date testField81 = new Date(100);
+
+      @ProtoField(number = 81/*, defaultValue = "1"*/)
+      public Date getTestField81() {
+         return testField81;
+      }
+
+      public void setTestField81(Date testField81) {
+         this.testField81 = testField81;
+      }
+
+      private Date[] testField82 = {new Date(100), new Date(200)};
+
+      @ProtoField(number = 82/*, defaultValue = "1"*/)
+      public Date[] getTestField82() {
+         return testField82;
+      }
+
+      public void setTestField82(Date[] testField82) {
+         this.testField82 = testField82;
+      }
+
+      private Instant testField83 = Instant.ofEpochMilli(100);
+
+      @ProtoField(number = 83/*, defaultValue = "1"*/)
+      public Instant getTestField83() {
+         return testField83;
+      }
+
+      public void setTestField83(Instant testField83) {
+         this.testField83 = testField83;
+      }
+
+      private Instant[] testField84 = {Instant.ofEpochMilli(100), Instant.ofEpochMilli(200)};
+
+      @ProtoField(number = 84/*, defaultValue = "1"*/)
+      public Instant[] getTestField84() {
+         return testField84;
+      }
+
+      public void setTestField84(Instant[] testField84) {
+         this.testField84 = testField84;
+      }
+
+      private byte[] testField85 = {1, 2, 3};
+
+      @ProtoField(number = 85, defaultValue = "85")
+      public byte[] getTestField85() {
+         return testField85;
+      }
+
+      public void setTestField85(byte[] testField85) {
+         this.testField85 = testField85;
+      }
+
+      private Byte[] testField86 = {1, 2, 3};
+
+      @ProtoField(number = 86, defaultValue = "86")
+      public Byte[] getTestField86() {
+         return testField86;
+      }
+
+      public void setTestField86(Byte[] testField86) {
+         this.testField86 = testField86;
+      }
    }
 
    @Test
