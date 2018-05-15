d0e8b1cc72e6cb242ed92df7f2b16a4c3a1cee22
Adrian Nistor <anistor@redhat.com>
2018-05-15 15:33:23 +0300

    Refactor DocumentationExtractor to avoid creation of vararg arrays at invocation

--- core/src/main/java/org/infinispan/protostream/annotations/impl/DocumentationExtractor.java
+++ core/src/main/java/org/infinispan/protostream/annotations/impl/DocumentationExtractor.java
@@ -11,21 +11,35 @@ import org.infinispan.protostream.annotations.ProtoDoc;
 final class DocumentationExtractor {
 
    /**
-    * Collect and concatenate the description text from the ProtoDoc.value of the given elements.
+    * Collect and concatenate the description text from the {@code @ProtoDoc.value} of the given elements.
     */
-   public static String getDocumentation(AnnotatedElement... elements) {
+   public static String getDocumentation(AnnotatedElement element1, AnnotatedElement element2) {
+      String doc1 = getDocumentation(element1);
+      String doc2 = getDocumentation(element2);
+
+      if (doc1 == null) {
+         return doc2;
+      }
+      if (doc2 == null) {
+         return doc1;
+      }
+      return doc1 + '\n' + doc2;
+   }
+
+   /**
+    * Collect and concatenate the description text from the {@code @ProtoDoc.value} of the given element.
+    */
+   public static String getDocumentation(AnnotatedElement element) {
       StringBuilder sb = null;
 
-      for (AnnotatedElement element : elements) {
-         for (ProtoDoc docAnnotation : element.getAnnotationsByType(ProtoDoc.class)) {
-            if (docAnnotation != null && !docAnnotation.value().isEmpty()) {
-               if (sb == null) {
-                  sb = new StringBuilder();
-               } else {
-                  sb.append('\n');
-               }
-               sb.append(docAnnotation.value());
+      for (ProtoDoc docAnnotation : element.getAnnotationsByType(ProtoDoc.class)) {
+         if (!docAnnotation.value().isEmpty()) {
+            if (sb == null) {
+               sb = new StringBuilder();
+            } else {
+               sb.append('\n');
             }
+            sb.append(docAnnotation.value());
          }
       }
 
