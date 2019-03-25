8b6c2f3a73f1b2c4f0b0c79c53e86f4f320e1dfa
Adrian Nistor <anistor@redhat.com>
2019-03-25 12:21:30 +0200

    Improve javadoc of @ProtoField

--- core/src/main/java/org/infinispan/protostream/annotations/ProtoField.java
+++ core/src/main/java/org/infinispan/protostream/annotations/ProtoField.java
@@ -26,15 +26,47 @@ public @interface ProtoField {
     */
    int number();
 
+   /**
+    * The Protobuf type of the field. If not specified, then the field type will be inferred based on the Java property
+    * type.
+    */
    Type type() default Type.MESSAGE;
 
+   /**
+    * Marks the field as required. Please use this sparingly as it will no longer be supported in Protobuf 3.0.
+    */
    boolean required() default false;
 
+   /**
+    * The name of the field. If not specified, then the name of the Java property is used instead.
+    */
    String name() default "";
 
+   /**
+    * The default value to assign to this field if it is found <em>missing</em> in the Protobuf data stream <em>during
+    * reading</em>.
+    * <p>
+    * The value is given in the form of a string that must obey correct syntax (as defined by Protobuf spec.) in order
+    * to be parsed into the actual value which may be of a different type than string.
+    * <p>
+    * This value has no significance during writing of a {@code null} field, ie. it does not get written into the data
+    * stream as a substitute for the missing value. It is expected that the reader of this Protobuf stream will do that
+    * when reading it back.
+    */
    String defaultValue() default "";
 
+   /**
+    * The actual concrete and instantiable Java type. This Class should be assignable to the property type. It should be
+    * used when the type is an interface or abstract class in order to designate the actual concrete class that must be
+    * instantiated by the marshaling layer when reading this from a data stream.
+    */
    Class<?> javaType() default void.class;
 
+   /**
+    * The actual concrete and instantiable implementation type of the Collection. This Class should only be specified if
+    * the Java property type is really a Collection and must be assignable to the property type. It should be used when
+    * the type of the Collection is an interface or an abstract class in order to designate the actual concrete class
+    * that must be instantiated by the marshaling layer when reading this from a data stream.
+    */
    Class<? extends Collection> collectionImplementation() default Collection.class;
 }
