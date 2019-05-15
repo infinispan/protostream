57224999b32ef9f64e4765f7cbc688fcac2ae701
Adrian Nistor <anistor@redhat.com>
2019-05-15 15:22:27 +0300

    Improve javadoc of HasModelElement

--- processor/src/main/java/org/infinispan/protostream/annotations/impl/processor/types/HasModelElement.java
+++ processor/src/main/java/org/infinispan/protostream/annotations/impl/processor/types/HasModelElement.java
@@ -3,13 +3,18 @@ package org.infinispan.protostream.annotations.impl.processor.types;
 import javax.lang.model.element.Element;
 
 /**
- * Some XElements have an associated javax.lang.model.element.Element, in which case they implement this interface in
- * order to expose the Element.
+ * Some {@link org.infinispan.protostream.annotations.impl.types.XElement}s have an associated
+ * javax.lang.model.element.Element, in which case they implement this interface in order to expose the Element.
  *
  * @author anistor@redhat.com
  * @since 4.3
  */
 public interface HasModelElement {
 
+   /**
+    * Get the {@link Element}, never {@code null}.
+    *
+    * @return the {@link Element}, never {@code null}
+    */
    Element getElement();
 }
