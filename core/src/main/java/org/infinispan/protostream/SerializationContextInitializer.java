648ace4ebb3f209d020cb8a71de2f44ac588f768
Adrian Nistor <anistor@redhat.com>
2020-07-30 12:32:36 +0300

    SerializationContext/SerializationContextImpl javadoc and error messages

--- core/src/main/java/org/infinispan/protostream/SerializationContextInitializer.java
+++ core/src/main/java/org/infinispan/protostream/SerializationContextInitializer.java
@@ -3,10 +3,14 @@ package org.infinispan.protostream;
 import java.io.UncheckedIOException;
 
 /**
- * An interface to be used in conjunction with {@link org.infinispan.protostream.annotations.AutoProtoSchemaBuilder}
- * annotation. Just create an abstract class implementing this interface or an interface extending it and annotate it
- * and the compiler (via a custom annotation processor) will generate a concrete implementation of it for you to use.
- * This is a compile-time equivalent of {@link org.infinispan.protostream.annotations.ProtoSchemaBuilder}.
+ * An interface to be usually used in conjunction with the {@link org.infinispan.protostream.annotations.AutoProtoSchemaBuilder}
+ * annotation. By creating an abstract class implementing this interface or an interface extending it and annotating it with
+ * {@link org.infinispan.protostream.annotations.AutoProtoSchemaBuilder} the compiler (via a custom annotation processor)
+ * will generate a concrete implementation for you to use. This mechanism is a compile-time alternative to
+ * {@link org.infinispan.protostream.annotations.ProtoSchemaBuilder} which works using runtime bytecode generation.
+ * <p>
+ * Manually written implementations of this interface are also allowed, but are of no special use with regard to
+ * ProtoStream library.
  *
  * @author anistor@redhat.com
  * @since 4.3
@@ -19,7 +23,10 @@ public interface SerializationContextInitializer {
    String getProtoFileName();
 
    /**
-    * Returns the contents of the proto file.
+    * Returns the contents of the proto file as a {@link String}. The returned value must be the same (equals) on each
+    * invocation. Implementations can return a constant or a value stored in memory but they are generally free to
+    * retrieve it from somewhere else, including the classpath, the disk, or even a mechanism that can potentially fail
+    * with an {@link UncheckedIOException}.
     *
     * @throws UncheckedIOException if the file contents cannot be retrieved
     */
