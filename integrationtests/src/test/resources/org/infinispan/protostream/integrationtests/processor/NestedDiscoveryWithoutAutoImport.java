8049c40ca92a33624f2f558b3bac81787fc7f5cd
Adrian Nistor <anistor@redhat.com>
2020-09-11 12:22:41 +0300

    IPROTO-150 Compile-time annotation processor should discover nested annotated classes even if autoImportClasses is false
    * more tests

--- integrationtests/src/test/resources/org/infinispan/protostream/integrationtests/processor/NestedDiscoveryWithoutAutoImport.java
+++ integrationtests/src/test/resources/org/infinispan/protostream/integrationtests/processor/NestedDiscoveryWithoutAutoImport.java
@@ -3,8 +3,18 @@ package test_nested_discovery_without_auto_import;
 import org.infinispan.protostream.*;
 import org.infinispan.protostream.annotations.*;
 
-@AutoProtoSchemaBuilder(includeClasses = {OuterMessage2.class, OuterMessage3.class}, autoImportClasses = false, schemaFilePath = "/")
-public interface NestedDiscoveryWithoutAutoImport extends SerializationContextInitializer {
+@AutoProtoSchemaBuilder(includeClasses = {OuterMessage2.class, OuterMessage3.class},
+      autoImportClasses = false, schemaFilePath = "/")
+interface NestedDiscoveryWithoutAutoImport extends SerializationContextInitializer {
+}
+
+@AutoProtoSchemaBuilder(basePackages = "test_nested_discovery_without_auto_import",
+      autoImportClasses = false, schemaFilePath = "/")
+interface NestedDiscoveryWithoutAutoImport2 extends SerializationContextInitializer {
+}
+
+@AutoProtoSchemaBuilder(autoImportClasses = false, schemaFilePath = "/")
+interface NestedDiscoveryWithoutAutoImport3 extends SerializationContextInitializer {
 }
 
 abstract class AbstractMessage {
@@ -26,8 +36,20 @@ class OuterMessage2 {
 
 class OuterMessage3 {
 
-   // this class is nested but not referenced from the outer class or the builder, so it does not get included
+   // this class is nested but not referenced from the outer class or the builder's includeClasses, so it does not get included
    static class InnerMessage3 extends AbstractMessage {
+      @ProtoField(number = 2)
+      String field2;
+   }
+
+   @ProtoField(number = 1)
+   String field1;
+}
+
+class OuterMessage4 {
+
+   // not referenced from the outer class or the builder, but it does get included because of basePackages
+   static class InnerMessage4 extends AbstractMessage {
       @ProtoField(number = 2)
       String field2;
    }
