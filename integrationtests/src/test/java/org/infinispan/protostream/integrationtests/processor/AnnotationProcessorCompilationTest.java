d202b48f6018cf0b15b42cee3af95cbd08a9f713
Adrian Nistor <anistor@redhat.com>
2020-12-17 15:05:41 +0200

    Extract reusable CompilationUtils class from AnnotationProcessorCompilationTest

--- integrationtests/src/test/java/org/infinispan/protostream/integrationtests/processor/AnnotationProcessorCompilationTest.java
+++ integrationtests/src/test/java/org/infinispan/protostream/integrationtests/processor/AnnotationProcessorCompilationTest.java
@@ -1,27 +1,18 @@
 package org.infinispan.protostream.integrationtests.processor;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import javax.tools.FileObject;
 import javax.tools.JavaFileObject;
 
-import org.infinispan.protostream.annotations.impl.processor.AutoProtoSchemaBuilderAnnotationProcessor;
 import org.junit.Test;
+
+import com.google.testing.compile.Compilation;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import com.google.common.io.Resources;
-import com.google.testing.compile.Compilation;
-import com.google.testing.compile.JavaFileObjects;
-
 import static com.google.testing.compile.CompilationSubject.assertThat;
-import static com.google.testing.compile.Compiler.javac;
 import static javax.tools.StandardLocation.CLASS_OUTPUT;
 import static javax.tools.StandardLocation.SOURCE_OUTPUT;
 
@@ -33,7 +24,7 @@ public class AnnotationProcessorCompilationTest {
 
    @Test
    public void testBasic() {
-      Compilation compilation = compile(
+      Compilation compilation = CompilationUtils.compile(
             "org/infinispan/protostream/integrationtests/processor/TestMessage.java",
             "org/infinispan/protostream/integrationtests/processor/AbstractFirstInitializer.java",
             "org/infinispan/protostream/integrationtests/processor/DependentInitializer.java"
@@ -53,56 +44,56 @@ public class AnnotationProcessorCompilationTest {
 
    @Test
    public void testBadSchemaFileName() {
-      Compilation compilation = compile("org/infinispan/protostream/integrationtests/processor/BadSchemaFileName.java");
+      Compilation compilation = CompilationUtils.compile("org/infinispan/protostream/integrationtests/processor/BadSchemaFileName.java");
       assertThat(compilation).succeeded();
       assertThat(compilation).hadWarningContaining("@AutoProtoSchemaBuilder.schemaFileName should end with '.proto'");
    }
 
    @Test
    public void testBadBasePackages() {
-      Compilation compilation = compile("org/infinispan/protostream/integrationtests/processor/BadBasePackages.java");
+      Compilation compilation = CompilationUtils.compile("org/infinispan/protostream/integrationtests/processor/BadBasePackages.java");
       assertThat(compilation).failed();
       assertThat(compilation).hadErrorContaining("@AutoProtoSchemaBuilder.value contains an invalid package name : \"test!!!\"");
    }
 
    @Test
    public void testNestedEnum() {
-      Compilation compilation = compile("org/infinispan/protostream/integrationtests/processor/TestEnumInitializer.java");
+      Compilation compilation = CompilationUtils.compile("org/infinispan/protostream/integrationtests/processor/TestEnumInitializer.java");
       assertThat(compilation).succeededWithoutWarnings();
       assertTrue(compilation.generatedFile(SOURCE_OUTPUT, "test_enum_initializer/TestEnumInitializerImpl.java").isPresent());
    }
 
    @Test
    public void testDependsOnCycle() {
-      Compilation compilation = compile("org/infinispan/protostream/integrationtests/processor/DependsOnCycle.java");
+      Compilation compilation = CompilationUtils.compile("org/infinispan/protostream/integrationtests/processor/DependsOnCycle.java");
       assertThat(compilation).failed();
       assertThat(compilation).hadErrorContaining("Illegal recursive dependency on test_depends_on_cycle.DependsOnCycle");
    }
 
    @Test
    public void testIncludeExcludeOverlap() {
-      Compilation compilation = compile("org/infinispan/protostream/integrationtests/processor/IncludeExcludeOverlap.java");
+      Compilation compilation = CompilationUtils.compile("org/infinispan/protostream/integrationtests/processor/IncludeExcludeOverlap.java");
       assertThat(compilation).failed();
       assertThat(compilation).hadErrorContaining("@AutoProtoSchemaBuilder.includeClasses and @AutoProtoSchemaBuilder.excludeClasses are mutually exclusive");
    }
 
    @Test
    public void testIncludeClassesVsBasePackagesConflict() {
-      Compilation compilation = compile("org/infinispan/protostream/integrationtests/processor/IncludeClassesVsBasePackagesConflict.java");
+      Compilation compilation = CompilationUtils.compile("org/infinispan/protostream/integrationtests/processor/IncludeClassesVsBasePackagesConflict.java");
       assertThat(compilation).failed();
       assertThat(compilation).hadErrorContaining("@AutoProtoSchemaBuilder.includeClasses and @AutoProtoSchemaBuilder.value/basePackages are mutually exclusive");
    }
 
    @Test
    public void testExcludeClassesVsBasePackagesConflict() {
-      Compilation compilation = compile("org/infinispan/protostream/integrationtests/processor/ExcludeClassesVsBasePackagesConflict.java");
+      Compilation compilation = CompilationUtils.compile("org/infinispan/protostream/integrationtests/processor/ExcludeClassesVsBasePackagesConflict.java");
       assertThat(compilation).failed();
       assertThat(compilation).hadErrorContaining("@AutoProtoSchemaBuilder.excludeClasses and @AutoProtoSchemaBuilder.value/basePackages are conflicting. Class 'test_exclude_classes_vs_base_packages_conflict.ExcludeClassesVsBasePackagesConflict.Msg' must belong to a base package.");
    }
 
    @Test
    public void testDiscoveryWithoutAutoImport() {
-      Compilation compilation = compile("org/infinispan/protostream/integrationtests/processor/DiscoveryWithoutAutoImport.java");
+      Compilation compilation = CompilationUtils.compile("org/infinispan/protostream/integrationtests/processor/DiscoveryWithoutAutoImport.java");
       assertThat(compilation).hadErrorContaining("Found a reference to class"
             + " test_discovery_without_auto_import.InnerMessage1 which was not explicitly included by"
             + " @AutoProtoSchemaBuilder and the combination of relevant attributes"
@@ -111,7 +102,7 @@ public class AnnotationProcessorCompilationTest {
 
    @Test
    public void testNestedDiscoveryWithoutAutoImport() {
-      Compilation compilation = compile("org/infinispan/protostream/integrationtests/processor/NestedDiscoveryWithoutAutoImport.java");
+      Compilation compilation = CompilationUtils.compile("org/infinispan/protostream/integrationtests/processor/NestedDiscoveryWithoutAutoImport.java");
       assertThat(compilation).succeededWithoutWarnings();
       assertTrue(compilation.generatedFile(SOURCE_OUTPUT, "test_nested_discovery_without_auto_import/NestedDiscoveryWithoutAutoImportImpl.java").isPresent());
       assertTrue(compilation.generatedFile(SOURCE_OUTPUT, "test_nested_discovery_without_auto_import/NestedDiscoveryWithoutAutoImport2Impl.java").isPresent());
@@ -139,90 +130,30 @@ public class AnnotationProcessorCompilationTest {
    /**
     * Asserts that the file contains a given expected string.
     */
-   static void assertFileContains(Optional<? extends FileObject> file, String string) {
+   private static void assertFileContains(Optional<? extends FileObject> file, String string) {
       assertFileContains(file.orElse(null), string);
    }
 
    /**
     * Asserts that the file does not contain a given string.
     */
-   static void assertFileDoesNotContain(Optional<? extends FileObject> file, String string) {
+   private static void assertFileDoesNotContain(Optional<? extends FileObject> file, String string) {
       assertFileDoesNotContain(file.orElse(null), string);
    }
 
    /**
     * Asserts that the file contains a given expected string.
     */
-   static void assertFileContains(FileObject file, String string) {
+   private static void assertFileContains(FileObject file, String string) {
       assertTrue("File " + file.getName() + " is expected to contain '"
-            + string + "' but it doesn't.", checkFileContainsString(file, string));
+            + string + "' but it doesn't.", CompilationUtils.checkFileContainsString(file, string));
    }
 
    /**
     * Asserts that the file does not contain a given string.
     */
-   static void assertFileDoesNotContain(FileObject file, String string) {
+   private static void assertFileDoesNotContain(FileObject file, String string) {
       assertFalse("File " + file.getName() + " is not expected to contain '"
-            + string + "' but it does.", checkFileContainsString(file, string));
-   }
-
-   private static boolean checkFileContainsString(FileObject file, String string) {
-      if (file == null) {
-         throw new IllegalArgumentException("The file argument must not be null");
-      }
-      if (string == null || string.length() == 0) {
-         throw new IllegalArgumentException("The string argument must not be null or empty");
-      }
-
-      String src;
-      try {
-         src = file.getCharContent(true).toString();
-      } catch (IOException ioe) {
-         throw new UncheckedIOException(ioe);
-      }
-
-      return src.contains(string);
-   }
-
-   /**
-    * Compiles several Java source files that are represented by resource files form classpath.
-    */
-   private static Compilation compile(String... resourceNames) {
-      if (resourceNames == null || resourceNames.length == 0) {
-         throw new IllegalArgumentException("resourceNames argument must not be null or empty");
-      }
-
-      List<JavaFileObject> files = new ArrayList<>(resourceNames.length);
-      for (String resourceName : resourceNames) {
-         files.add(createJavaFileObject(resourceName));
-      }
-
-      return javac()
-            .withProcessors(new AutoProtoSchemaBuilderAnnotationProcessor())
-            .compile(files);
-   }
-
-   /**
-    * Loads a resource file from classpath that is expected to be a Java source file and creates a JavaFileObject.
-    */
-   private static JavaFileObject createJavaFileObject(String resourceName) {
-      String fullyQualifiedName;
-      String suffix = ".java";
-      if (resourceName.endsWith(suffix)) {
-         fullyQualifiedName = resourceName.substring(0, resourceName.length() - suffix.length());
-      } else {
-         throw new IllegalArgumentException("The resource must be a Java source file and the file name must end with '.java' suffix");
-      }
-      fullyQualifiedName = fullyQualifiedName.replace('/', '.');
-
-      String source;
-      try {
-         source = Resources.asByteSource(Resources.getResource(resourceName))
-               .asCharSource(StandardCharsets.UTF_8)
-               .read();
-      } catch (IOException ioe) {
-         throw new UncheckedIOException(ioe);
-      }
-      return JavaFileObjects.forSourceString(fullyQualifiedName, source);
+            + string + "' but it does.", CompilationUtils.checkFileContainsString(file, string));
    }
 }
