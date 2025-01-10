cfbf12f1b9ca4ba8735393ab355a3613db1c0043
Tristan Tarrant <ttarrant@redhat.com>
2025-01-10 16:07:27 +0100

    [#394] Don't use generics when marshalling collections
    * A temporary workaround for the issue. Full support for generic
      parameter types will need to be added to type mirroring

--- integrationtests/src/test/java/org/infinispan/protostream/integrationtests/processor/UserSerializationContextInitializer.java
+++ integrationtests/src/test/java/org/infinispan/protostream/integrationtests/processor/UserSerializationContextInitializer.java
@@ -4,12 +4,14 @@ import org.infinispan.protostream.GeneratedSchema;
 import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
 import org.infinispan.protostream.annotations.ProtoSyntax;
 import org.infinispan.protostream.domain.Address;
+import org.infinispan.protostream.domain.PairAdapter;
 import org.infinispan.protostream.domain.User;
 
 @AutoProtoSchemaBuilder(
       includeClasses = {
             User.class,
             Address.class,
+            PairAdapter.class
       },
       schemaFileName = "user.proto",
       schemaFilePath = "proto/",
