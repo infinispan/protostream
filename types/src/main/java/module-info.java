9839f0e0add00aba04ac4f5674213781f410d451
Tristan Tarrant <tristan@infinispan.org>
2025-08-05 11:20:31 +0200

    [#502] Export/open additional packages in protostream-types

--- types/src/main/java/module-info.java
+++ types/src/main/java/module-info.java
@@ -3,4 +3,8 @@ module org.infinispan.protostream.types {
    requires org.jboss.logging;
    requires java.compiler;
    opens org.infinispan.protostream.types;
+   opens org.infinispan.protostream.types.java;
+   opens org.infinispan.protostream.types.protobuf;
+   exports org.infinispan.protostream.types.java;
+   exports org.infinispan.protostream.types.protobuf;
 }
